32853deafcf27612c2b5d2025e34264d6d07508a
Steve <steveleinhoss@gmail.com>
2024-05-07 22:01:24 +0200

    Basic Websocket Setup

--- app.vue
+++ app.vue
@@ -1,5 +1,34 @@
 <template>
   <div>
-    <NuxtWelcome />
+      <button @click="handleClick">
+          click
+      </button>
   </div>
 </template>
+
+<script setup lang="ts">
+let ws: WebSocket | undefined;
+const connect = async () => {
+    const isSecure = location.protocol === "https:";
+    const url = (isSecure ? "wss://" : "ws://") + location.host + "/_ws";
+    if (ws) {
+        ws.close();
+    }
+
+    ws = new WebSocket(url);
+
+    ws.addEventListener("message", (event) => {
+        console.log(event.data)
+    });
+
+    await new Promise((resolve) => ws!.addEventListener("open", resolve));
+};
+
+const handleClick = () => {
+    ws!.send("ping");
+}
+
+onMounted(async () => {
+    connect();
+});
+</script>
